fe4814c7d73b8d4cce94fa64e22134aea927677f
Braien Machado de Paula <87545637+braien-machado@users.noreply.github.com>
2024-05-03 00:42:09 +0000

    finish assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function calculateRank(wins, defeats) {
+    const difference = wins - defeats
+    let level
+
+    if (difference >= 101) {
+        level = 'Imortal'
+    } else if (difference >= 91) {
+        level = 'Lendário'
+    } else if (difference >= 81) {
+        level = 'Diamante'
+    } else if (difference >= 51) {
+        level = 'Ouro'
+    } else if (difference >= 21) {
+        level = 'Prata'
+    } else if (difference >= 11) {
+        level = 'Bronze'
+    } else {
+        level = 'Ferro'
+    }
+
+    console.log(`O Herói tem de saldo de ${difference} está no nível de ${level}`)
+}
+
+calculateRank(20,5)
